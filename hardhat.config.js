4587e869207cb5153e28e2b6c9032bf2064c1342
Taufik <1012824521@qq.com>
2024-07-27 11:10:05 +0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+    },
+    solidity: "0.8.8",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+
+    ethers: {
+        defaultProvider: function () {
+            return new ethers.providers.JsonRpcProvider()
+        },
+    },
+}
